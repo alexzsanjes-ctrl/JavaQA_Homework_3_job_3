d74c82f8f351e8514bb786145fc509388eb754a7
Александр Саломанин <alexzsanjes@gmail.com>
2025-12-17 23:18:40 +0700

    modify

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,8 @@
+public class CreditPaymentService {
+    public int Calculate (int Credit, int Term, double Rate) {
+        double Percent = (Rate/100)/12; // процентная ставка в месяц
+        double Payment; // Ежемесячный платёж
+        Payment = Credit*(Percent* Math.pow((1+Percent),Term))/((Math.pow((1+Percent), Term))-1); // Расчёт ежемесячного платежа
+        return (int) Payment;
+    }
+}
